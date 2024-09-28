3a3813ad2deec4142e545b6f54c13ecf602bd647
harishsharmacoupa <harish.sharma@coupa.com>
2024-09-29 01:23:59 +0530

    more updates

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
-    validates :name, :ticker_symbol , presence: true
+    validates :name, :ticker , presence: true
     def self.new_lookup(ticker_symbol)
         finnhub_client = FinnhubRuby::DefaultApi.new
         begin
