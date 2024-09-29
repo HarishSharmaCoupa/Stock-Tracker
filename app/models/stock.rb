18d3870bbf6da84c9e39a78f2df1f81666931884
harishsharmacoupa <harish.sharma@coupa.com>
2024-09-29 18:28:31 +0530

    added stock adding and removal functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,7 @@ class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
     validates :name, :ticker , presence: true
+
     def self.new_lookup(ticker_symbol)
         finnhub_client = FinnhubRuby::DefaultApi.new
         begin
@@ -9,6 +10,9 @@ class Stock < ApplicationRecord
         rescue => exception
             return nil
         end
+    end
 
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
     end
 end
